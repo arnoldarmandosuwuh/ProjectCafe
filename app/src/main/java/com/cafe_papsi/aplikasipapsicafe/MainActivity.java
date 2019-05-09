89e69d41b4db024f3aefa787a0f3e4717dc2614f
Arnold Armando Suwuh <arnoldarmando07@gmail.com>
2019-05-09 12:40:55 +0700

    Fragment Beranda

--- app/src/main/java/com/cafe_papsi/aplikasipapsicafe/MainActivity.java
+++ app/src/main/java/com/cafe_papsi/aplikasipapsicafe/MainActivity.java
@@ -111,8 +111,6 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
-
         } else if (id == R.id.nav_logout) {
             sharedPrefManager = new SharedPrefManager(this);
 
